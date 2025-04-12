6e8e12b3182bc3975062105408e81740c726045f
Dirtylowdown <cactusjacknf@gmail.com>
2025-04-12 16:03:47 -0500

    Delete src directory
    Terminate 
    Termination
    Void 
    Stop
    Close 
    Done
    Delete 
    End

--- src/Profilers/CommonMonitorProfiler/Logging/NullLogger.cpp
+++
@@ -1,6 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#include "NullLogger.h"
-
-std::shared_ptr<NullLogger> NullLogger::Instance = std::make_shared<NullLogger>();
